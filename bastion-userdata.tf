c5f91926512bf107743a231d68a9cd163b4db5c1
prashantkumashi <55656842+prashantkumashi@users.noreply.github.com>
2023-08-02 23:41:41 +0530

    Create bastion-userdata.tf

---
+++ bastion-userdata.tf
@@ -0,0 +1,31 @@
+locals {
+  bastion-userdata = <<USERDATA
+MIME-Version: 1.0
+Content-Type: multipart/mixed; boundary="==MYBOUNDARY=="
+
+--==MYBOUNDARY==
+Content-Type: text/x-shellscript; charset="us-ascii"
+
+#!/bin/bash -xe
+echo "Running custom user data script" > /tmp/me.txt
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install --bin-dir /usr/bin --install-dir /usr/aws-cli --update
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/aws-iam-authenticator
+chmod +x ./aws-iam-authenticator
+mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+
+yum install -y amazon-ssm-agent
+echo "yum'd agent" >> /tmp/me.txt
+yum update -y
+systemctl enable amazon-ssm-agent && systemctl start amazon-ssm-agent
+
+
+date >> /tmp/me.txt
+
+--==MYBOUNDARY==--
+USERDATA
+}
