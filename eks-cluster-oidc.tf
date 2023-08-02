425dd013b6b9d913fd0da06478eb7867b059ff25
prashantkumashi <55656842+prashantkumashi@users.noreply.github.com>
2023-08-02 23:07:11 +0530

    Update eks-cluster-oidc.tf

--- eks-cluster-oidc.tf
+++ eks-cluster-oidc.tf
@@ -1,13 +1,13 @@
 ### OIDC Provider
 data "tls_certificate" "cluster" {
-  url = aws_eks_cluster.empyern.identity.0.oidc.0.issuer
+  url = aws_eks_cluster.sampleprivatecluster.identity.0.oidc.0.issuer
 }
-resource "aws_iam_openid_connect_provider" "empyern-cluster" {
+resource "aws_iam_openid_connect_provider" "sampleprivatecluster-cluster" {
   client_id_list = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.cluster.certificates.0.sha1_fingerprint]
-  url = aws_eks_cluster.empyern.identity.0.oidc.0.issuer
+  url = aws_eks_cluster.sampleprivatecluster.identity.0.oidc.0.issuer
 }
 
 output oidc_provider_arn {
-  value=aws_iam_openid_connect_provider.empyern-cluster.arn
+  value=aws_iam_openid_connect_provider.sampleprivatecluster-cluster.arn
 }
